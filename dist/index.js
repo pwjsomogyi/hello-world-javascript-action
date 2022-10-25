cebd5227d0dab56aafd65fe4654e277623a76713
pwjsomogyi <116604354+pwjsomogyi@users.noreply.github.com>
2022-10-25 10:29:21 -0700

    Updated action

--- dist/index.js
+++ dist/index.js
@@ -9696,6 +9696,10 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const commitId = github.context.payload.commits[0].id
+  console.log(`The commit id: ${commitURL}`);
+  const commitURL = github.context.payload.commits[0].url
+  console.log(`The commit URL: ${commitURL}`);
 } catch (error) {
   core.setFailed(error.message);
 }
